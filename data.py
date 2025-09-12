21f30c6f30a3b7f81414c473a085339079f8137a
Arpan Mahara <amaha038@fiu.edu>
2025-09-11 20:00:17 -0400

    udpated data file

--- data.py
+++ data.py
@@ -1,15 +1,16 @@
-from torch.utils.data import Dataset
+# data.py
+from torch.utils.data import Dataset, DataLoader, DistributedSampler
 from torchvision.datasets import ImageFolder
 import torchvision.transforms as transforms
-from torch.utils.data import DataLoader, DistributedSampler
+import torch.distributed as dist
 
 class Satellite_Dataset(Dataset):
     def __init__(self, dir_path, transform=None):
         self.data = ImageFolder(dir_path, transform=transform)
-    
+
     def __len__(self):
         return len(self.data)
-    
+
     def __getitem__(self, idx):
         return self.data[idx]
 
@@ -22,5 +23,22 @@ def get_transforms():
 
 def prepare_dataloader(path, transform, batch_size, is_train=True):
     dataset = Satellite_Dataset(path, transform=transform)
-    sampler = DistributedSampler(dataset, shuffle=is_train)
-    return DataLoader(dataset, batch_size=batch_size, sampler=sampler, num_workers=4, pin_memory=True)
+
+    use_ddp = dist.is_available() and dist.is_initialized()
+    sampler = DistributedSampler(
+        dataset,
+        shuffle=is_train,
+        drop_last=is_train
+    ) if use_ddp else None
+
+    # shuffle only when not using a sampler
+    return DataLoader(
+        dataset,
+        batch_size=batch_size,
+        sampler=sampler,
+        shuffle=(sampler is None and is_train),
+        num_workers=4,
+        pin_memory=True,
+        persistent_workers=True if 4 > 0 else False,
+        drop_last=is_train
+    )
